95186f6f391090603e6d08ed723ca0cf7e9f5bd2
Sergio Cárdenas <se.cardenas@uniandes.edu.co>
2018-10-27 18:07:37 -0500

    imports in server main.js

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 /*global ServiceConfiguration, process*/
 import { Meteor } from 'meteor/meteor';
+import '../imports/api/rooms.js';
+import '../imports/api/Spotify.js';
+import '../imports/api/users.js';
 
 Meteor.startup(() => {
   // code to run on server at startup  
