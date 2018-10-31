9f82ac051b4dec44865e44c9efecfdbcfada8159
Ricardo Enrique Gonzalez Peñuela <rgonzalezp1115@gmail.com>
2018-10-31 09:39:49 -0500

    Update main.js

--- server/main.js
+++ server/main.js
@@ -4,6 +4,13 @@ import '../imports/api/rooms.js';
 import '../imports/api/Spotify.js';
 import '../imports/api/users.js';
 
+//Existe un bug tratando de autorizar un usuario mediante Facebook. Steps to reproduce bug: 
+//1. Open http://blendify.herokuapp.com/ with firefox in mac
+//2. Click on get started
+//3. Click sign in with facebook
+//4. The window changes to a blank screen and stays that way.
+//5. If you reload the screen and try to click get started again, a blank window opens.
+
 Meteor.startup(() => {
   // code to run on server at startup  
 
